c18c1a167f0942535379851875196b2651d81a1e
HaseFlrn <florianhase12@gmail.com>
2023-04-14 00:03:33 +0200

    copy dl-code

--- memory_opt_learn.py
+++ memory_opt_learn.py
@@ -81,4 +81,122 @@ def get_data(batch_size, img_height, img_width, sample_size):
     return train_ds, val_ds
 
 
+def generate_model(layers):
+    model = Sequential()
+    for layer in layers:
+        model.add(layer)
+    return model
 
+
+def train_model(model, learning_rate, train_ds, val_ds, epoch_range):
+    opt = Adam(learning_rate=learning_rate)
+    model.compile(optimizer=opt, loss=tf.keras.losses.SparseCategoricalCrossentropy(
+    ), metrics=['accuracy'])
+
+    history = model.fit(train_ds, epochs=epoch_range, validation_data=val_ds)
+
+    return model, history
+
+
+def get_metrics(model, labels, x_val, y_val):
+    predictionOdds = model.predict(x_val)
+    predictions = []
+    for pred in predictionOdds:
+        predictions.append(list(pred).index(max(pred)))
+
+    predictions = np.array(predictions)
+    predictions = predictions.reshape(1, -1)[0]
+    return classification_report(y_val, predictions, target_names=labels)
+
+
+def run_neural_network(img_size, processed_images, labels, epoch_range, learning_rate, layers):
+
+    train_ds, val_ds = get_data(
+        batch_size, img_height=img_size, img_width=img_size, sample_size=processed_images)
+
+    model = generate_model(layers)
+    print(model.summary())
+
+    model, history = train_model(
+        model, learning_rate, train_ds, val_ds, epoch_range)
+    x_val, y_val = val_ds
+    metrics = get_metrics(model, labels, x_val, y_val)
+
+    return history, metrics
+
+
+def main():
+    resultPath = os.path.join(base_dir, 'NeuralNetwork_Results.xlsx')
+    cities = get_class_names()
+    image_count = [100, 1000, 2000]  # alles >1500 crasht wegen memory overflow
+    resolution = [244, 450, 1000]
+    learning_rate = [0.000001, 0.00001, 0.0001]
+    epoch_range = 100
+
+    results = pd.DataFrame(columns=['ID', 'Netzwerk', 'Anzahl Städte', 'Anzahl Bilder', 'Bildauflösung (px)', 'Lernquote',
+                                    'Train Acc', 'Val Acc', 'Train Loss', 'Val Loss', 'Metriken'])
+    results = results.set_index('ID')
+    rowId = 0
+
+    for i, _ in enumerate(cities):
+        if(i == 0):
+            continue
+        for images in image_count:
+            for res in resolution:
+                if images > 1000 and res > 450:
+                    continue
+                for lr in learning_rate:
+                    # needs res for initialization
+                    network1 = [
+                        Conv2D(32, 3, padding="same", activation="relu",
+                               input_shape=(res, res, 3)),
+                        Conv2D(32, 3, padding="same", activation="relu"),
+                        MaxPool2D(),
+                        ReLU(),
+                        Conv2D(64, 3, padding="same", activation="relu"),
+                        Conv2D(64, 3, padding="same", activation="relu"),
+                        MaxPool2D(),
+                        DropOut(0.4),
+                        Flatten(),
+                        Dense(128, activation="relu"),
+                        Dense(len(cities), activation="softmax")
+                    ]
+                    network2 = [
+                        Conv2D(32, 3, padding="same", activation="relu",
+                               input_shape=(res, res, 3)),
+                        MaxPool2D(),
+                        Conv2D(32, 3, padding="same", activation="relu"),
+                        MaxPool2D(),
+                        Conv2D(64, 3, padding="same", activation="relu"),
+                        MaxPool2D(),
+                        Dropout(0.4),
+                        Flatten(),
+                        Dense(128, activation="relu"),
+                        Dense(len(cities), activation="softmax")
+                    ]
+                    networks = [network1, network2]
+                    for j, network in enumerate(networks):
+
+                        history, metrics = run_neural_network(
+                            res, images, cities[:i+1], epoch_range, lr, network)
+
+                        results.loc[rowId] = ['Netzwerk ' + str(j+1), i+1, images, res, lr, history.history['accuracy'],
+                                              history.history['val_accuracy'], history.history['loss'],
+                                              history.history['val_loss'], metrics]
+
+                        print("Error on network" + str(j+1) + ":\n\timageCnt: " +
+                              str(images) + "\n\tresolution: " + str(res))
+                        results.loc[rowId] = ['Netzwerk ' + str(j+1), i+1, images, res, lr, "",
+                                              "", "",
+                                              "", ""]
+                        rowId = rowId + 1
+    try:
+        results.to_excel(resultPath)
+    except:
+        print("Didn't write to file!")
+    return results  # safe path if write doesn't work
+
+
+# main()
+x, y = get_data(1, 200, 200, 5)
+print(y)
